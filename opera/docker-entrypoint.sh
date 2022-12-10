ae06edadc59b10ee7f8520ef95afe9f63422b578
Thorsten Behrens <yorickdowne@gmail.com>
2022-12-10 16:44:43 +0000

    Add log level and prune-opera command

--- opera/docker-entrypoint.sh
+++ opera/docker-entrypoint.sh
@@ -20,4 +20,37 @@ if [[ -f "/var/lib/opera/setupdone" && -f "$GENESIS" ]]; then
   rm "$GENESIS"
 fi
 
-exec "$@"
+# Set verbosity
+shopt -s nocasematch
+case ${LOG_LEVEL} in
+  error)
+    __verbosity="--verbosity 1"
+    ;;
+  warn)
+    __verbosity="--verbosity 2"
+    ;;
+  info)
+    __verbosity="--verbosity 3"
+    ;;
+  debug)
+    __verbosity="--verbosity 4"
+    ;;
+  trace)
+    __verbosity="--verbosity 5"
+    ;;
+  *)
+    echo "LOG_LEVEL ${LOG_LEVEL} not recognized"
+    __verbosity=""
+    ;;
+esac
+
+if [ -f /var/lib/opera/prune-marker ]; then
+  rm -f /var/lib/opera/prune-marker
+# Word splitting is desired for the command line parameters
+# shellcheck disable=SC2086
+  exec "$@" snapshot prune-state
+else
+# Word splitting is desired for the command line parameters
+# shellcheck disable=SC2086
+  exec "$@" ${__verbosity}
+fi
